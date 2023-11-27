f5a106d4c6fad424e18f1e7cfc42e7a4e4cf3da8
Hanane MOUFTAOU <mouftaouhanane@gmail.com>
2023-11-27 18:20:45 +0100

    first commit

--- app/app/Http/Controllers/HomeController.php
+++ app/app/Http/Controllers/HomeController.php
@@ -17,60 +17,26 @@ use Exception;
 
 class HomeController extends Controller
 {
-    //
+    //page d'accueil
     public function show()
     {
         return view('welcome');
     }
 
-    public function showProfil()
-    {
-        $user  = User::where('id', session('user_id'))->first();
-
-        return view('profil', [
-            "name" => $user->username, "email" => $user->email, "avatar" => $user->avatar, "user_id" => session('user_id')
-        ]);
-    }
-    public function listingUser()
-    {
-        $users = User::whereNull('is_deleted')->get();
-
-        return view('users', ["users" => $users]);
-    }
-
-
+    //page de login
     public function showLogin()
     {
         return view('login');
     }
 
-    public function showRiddle()
-    {
-        log::error(session('name'));
-        $userScore = UserWord::where('users_id', session('user_id'))->sum('score');
-        if ($userScore) {
-
-            return view('word', ['name' => session('name'), 'scores' => $userScore]);
-        } else {
-            $score =  0;
-            return view('word', ['name' => session('name'), 'scores' => $score]);
-        }
-    }
-
-    public function showRegister()
+    //page de register
+    public function register()
     {
         return view('register');
     }
 
-    public function showPageUProfil()
-    {
-        $user_id = session('user_id'); // Obtenez l'identifiant de l'utilisateur depuis la session
 
-        return view('updateProfil', ["user_id" => $user_id]);
-    }
-
-
-
+    //register user
     public function registerUser(Request $request)
     {
         // Validation des données
@@ -100,12 +66,7 @@ class HomeController extends Controller
         }
     }
 
-
-    public function register()
-    {
-        return view('register');
-    }
-
+    // login de l'utilisateur
     public function loginUser(Request $request)
     {
         try {
@@ -131,6 +92,8 @@ class HomeController extends Controller
             log::error($ex);
         }
     }
+
+    // Modeification profil
     public function updateProfil(Request $request, $id)
     {
         log::error($id);
@@ -166,171 +129,42 @@ class HomeController extends Controller
         }
     }
 
-
-    public function riddle(Request $request)
+    //compte
+    public function showProfil()
     {
-        try {
-            $validatedData = $request->validate([
-                'date' => 'required|date',
-            ]);
-            // Validation des données
-            $first_name = $request->input('first-name');
-            $second_name = $request->input('second-name');
-            $third_name = $request->input('third-name');
-            $four_name = $request->input('four-name');
-            $five_name = $request->input('five-name');
-            $date = $validatedData['date'];
-            $word = $first_name . $second_name . $third_name . $four_name . $five_name;
-            $date1 = Carbon::parse($date);
+        $user  = User::where('id', session('user_id'))->first();
 
-            $user = User::where('id',  session('user_id'))->first();
-            if ($user->lastdate !== null) {
-                $date2 = Carbon::parse($user->lastdate);
-                if ($date1->equalTo($date2)) {
-                    return redirect()->route('show-riddle')->with(
-                        'error',
-                        'Désolé, Vous avec déja deviné le mots de cette date.Choisissez une autre date pour deviner le mots du jour '
-                    );
-                }
-            }
-            // Récupérer le nombre d'essais restants pour cette date
-            $attemptsLeftKey = 'attemptsLeft_' . $date1->toDateString();
-            $attemptsLeft = $request->session()->get($attemptsLeftKey, 8);
-            $foundWord = Word::where("word",  $word)->where("date",  $date1->toDateString())->first();
-            $wordDay = Word::where("date",  $date1->toDateString())->first();
-
-            if ($foundWord) {
-                $score = new UserWord();
-                $score->words_id = $foundWord->id;
-                $score->users_id =  session('user_id');
-                $score->score = 1;
-                $score->found_word = True;
-                $score->save();
-
-                $user->lastdate =  $date;
-                $user->save();
-
-                $request->session()->put('word_id', $foundWord->id);
-
-                return redirect()->route('show-like-word')->with(['message' => 'Bravo vous avez un point de plus', 'word' => $word]);
-            }
-
-            if ($attemptsLeft > 1) {
-                // Il reste des essais, décrémentez le nombre d'essais restants
-                $attemptsLeft--;
-                $request->session()->put($attemptsLeftKey, $attemptsLeft);
-                return  redirect()->route('show-riddle')->with('error', 'Désolé, essayez à nouveau. Il vous reste ' . $attemptsLeft . ' essais.');
-            } else {
-
-                // Plus d'essais, le joueur a perdu
-                $request->session()->forget($attemptsLeftKey); // Supprimez la clé de session
-
-                $score = new UserWord();
-                $score->words_id = $wordDay->id;
-                $score->users_id =  session('user_id');
-                $score->score = 0;
-                $score->found_word = False;
-                $score->save();
-
-                $user->lastdate =  $date;
-                $user->save();
-                Log::error($wordDay->word);
-                return redirect()->route('show-like-word')->with(['error', 'Désolé, vous avez épuisé vos essais. Le  mots  du jour est', 'word' => $wordDay->word]);
-            }
-        } catch (Exception $ex) {
-            log::error($ex);
-        }
+        return view('profil', [
+            "name" => $user->username, "email" => $user->email, "avatar" => $user->avatar, "user_id" => session('user_id')
+        ]);
     }
 
-    public function showLikeWordPage()
+    //listing user
+    public function listingUser()
     {
+        $users = User::whereNull('is_deleted')->get();
 
-        return view("likePage", ['name' => session('name')]);
-    }
-
-    public function likeWord(Request $request)
-    {
-        try {
-            // Validation des données
-            $request->validate([
-                'response' => 'required',
-                'note' => 'required|numeric',
-            ]);
-
-            $response = $request->input('response');
-            $note = $request->input('note');
-            $user_id = session('user_id');
-            $word = $request->session()->get('word_id');
-            log::error($word);
-
-
-            $likeWord = UserWord::where('users_id', $user_id)->where('words_id', $word)->first();
-            log::error("likeword");
-            log::error($likeWord);
-            log::error("response");
-            log::error($response);
-            log::error("note");
-            log::error($note);
-
-            if (!$likeWord) {
-                return redirect()->route('show-riddle')->with('error', 'Objet UserWord non trouvé.');
-            }
-
-            if ($response === 'true') {
-                $likeWord->update(['note' => $note, 'response' => 1]);
-            } elseif ($response === 'false') {
-                $likeWord->update(['note' => $note, 'response' => 0]);
-            }
-
-
-
-            return redirect()->route('show-riddle');
-        } catch (Exception $ex) {
-            log::error($ex);
-            return redirect()->route('show-riddle')->with('error', 'Une erreur s\'est produite.');
-        }
-    }
-
-
-    public function userScore()
-    {
-        try {
-            $userId = session('user_id');
-
-            // Récupérer les amis de l'utilisateur
-            $user = User::find($userId);
-            $friends = $user->friends;
-
-            // Ajouter l'utilisateur lui-même à la liste des amis (si nécessaire)
-            $friends[] = $userId;
-
-            // Récupérer les scores des amis
-            $userScores = [];
-            foreach ($friends as $friendId) {
-                $friend = User::find($friendId);
-                $totalScore = UserWord::where('users_id', $friendId)->sum('score');
-
-                $userScores[] = [
-                    'friend' => $friend,  // Contient l'objet User de l'ami
-                    'totalScore' => $totalScore,
-                ];
-            }
-
-            return view('users_score', ['userScores' => $userScores]);
-        } catch (Exception $ex) {
-            log::error($ex);
-        }
+        return view('users', ["users" => $users]);
     }
 
 
 
 
 
-    public function historique($id)
+
+
+    public function showRegister()
     {
-        $list = UserWord::where("users_id", $id)->get();
-        return view('historique', ['list' => $list]);
+        return view('register');
     }
+
+    public function showPageUProfil()
+    {
+        $user_id = session('user_id'); // Obtenez l'identifiant de l'utilisateur depuis la session
+
+        return view('updateProfil', ["user_id" => $user_id]);
+    }
+
 
     public function deleteUser($id)
     {
@@ -340,29 +174,10 @@ class HomeController extends Controller
     }
 
 
-    public function addFriend($id)
-    {
-        $friend = $id;
 
-        $users_friend = User::where('id', session('user_id'))->first();
-        log::error($users_friend);
 
-        if ($users_friend->friends === null) {
-            $users_friend->friends = [];
-        }
 
-        $friendsCopy = $users_friend->friends;
 
-        if (!in_array($friend, $friendsCopy)) {
-            $friendsCopy[] = $friend;
 
-            $users_friend->friends = $friendsCopy;
 
-            $users_friend->save();
-
-            return redirect()->back()->with('success', 'Demande d\'ami ajoutée avec succès.');
-        }
-
-        return redirect()->back()->with('error', 'Cet utilisateur est déjà dans votre liste d\'amis.');
-    }
 }
--- app/routes/web.php
+++ app/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DevinetteController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,22 +17,24 @@ use App\Http\Controllers\HomeController;
 */
 
 Route::get('/', [HomeController::class, 'show']);
+Route::post('/login', [HomeController::class, 'loginUser'])->name('login');
 Route::get('/register', [HomeController::class, 'register'])->name('register');
 Route::post('/register-user', [HomeController::class, 'registerUser'])->name('register-user');
 Route::put('/update-profil/{id}', [HomeController::class, 'updateProfil'])->name('update-profil');
 Route::get('/delete-profil/{id}',  [HomeController::class, 'deleteUser'])->name('delete-profil');
 Route::get('/show-login',  [HomeController::class, 'showLogin'])->name('show-login');
 Route::get('/show-profil',  [HomeController::class, 'showProfil'])->name('show-profil');
-Route::get('/show-riddle',  [HomeController::class, 'showRiddle'])->name('show-riddle');
 Route::get('/show-page-update-profil', [HomeController::class, 'showPageUProfil'])->name('show-update-profil');
-Route::get('/show-like-word', [HomeController::class, 'showLikeWordPage'])->name('show-like-word');
-Route::post('/login', [HomeController::class, 'loginUser'])->name('login');
-Route::post('/user-riddle', [HomeController::class, 'riddle'])->name('user-riddle');
-Route::post('/like-word', [HomeController::class, 'likeWord'])->name('like-word');
-Route::get('/show-users-score', [HomeController::class, 'userScore'])->name('show-users-score');
-Route::get('/historique/{id}', [HomeController::class, 'historique'])->name('historique');
 Route::get('/users', [HomeController::class, 'listingUser'])->name('users');
-Route::get('/users-friend/{id}', [HomeController::class, 'addFriend'])->name('users-friend');
+
+
+Route::get('/show-riddle',  [DevinetteController::class, 'showRiddle'])->name('show-riddle');
+Route::get('/show-like-word', [DevinetteController::class, 'showLikeWordPage'])->name('show-like-word');
+Route::post('/user-riddle', [DevinetteController::class, 'riddle'])->name('user-riddle');
+Route::post('/like-word', [DevinetteController::class, 'likeWord'])->name('like-word');
+Route::get('/show-users-score', [DevinetteController::class, 'userScore'])->name('show-users-score');
+Route::get('/historique/{id}', [DevinetteController::class, 'historique'])->name('historique');
+Route::get('/users-friend/{id}', [DevinetteController::class, 'addFriend'])->name('users-friend');
 
 
 
